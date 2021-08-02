5ac121c58e924843c8a01a82c9b7608aaaede1fa
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-03 00:29:48 +0600

    switch case file added

---
+++ switch.js
@@ -0,0 +1,22 @@
+var color = 'orange';
+switch (color) {
+    case 'red':
+        console.log('Color is red');
+        break;
+    case 'white':
+        console.log('Color is white');
+        break;
+    case 'green':
+        console.log('Color is green');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+        break;
+    case 'orange':
+    case 'yellow':
+        console.log('Color is yellow');
+        break;
+
+    default:
+        console.log("Color is Black");
+}
