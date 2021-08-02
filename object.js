c5c2603f51efb0d76a8d58e551195e761b4bda31
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-03 00:00:43 +0600

    object file added

---
+++ object.js
@@ -0,0 +1,14 @@
+var student1 = {
+    name: 'Piasha batasha',
+    height: "5'7",
+    role: 1,
+    marks: 88
+}
+
+var mobile = {
+    name: 'POCO X3 Pro',
+    color: 'gray',
+    price: 24500,
+    screenSize: 6,
+    storage: 128
+}
