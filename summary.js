0ed6c52d5c72f305d0df7ce290ac28a9696dbf49
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-03 01:04:47 +0600

    summary file added

---
+++ summary.js
@@ -0,0 +1,12 @@
+
+var phones = ['iphone', 'shaomi', 'samsung', 'lg', 'htc'];
+
+// check oppo not exists in an array 
+if(phones.indexOf('oppo') == -1){
+    console.log('Oppo is not founded');
+}
+
+// check string exists in an array
+if (phones.indexOf('lg') >= 0){
+    console.log('Lg is a good phone');
+}
