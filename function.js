f4c160a215a559eeced2b890f81f2adb013aef02
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-02 22:34:09 +0600

    function file added

---
+++ function.js
@@ -0,0 +1,12 @@
+// function declaration
+function startFan(){
+    console.log('Fan chala choto');
+}
+
+// Call the function -function jekono jaygay call kora jay,kintu loop ak jaygay e run hoy
+startFan();
+
+console.log('Barabari kom kor');
+console.log('bichar dimu kintu');
+console.log('mair khabi');
+startFan();
